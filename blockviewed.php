e67d09394ef94230ca17688f6caca0435789e019
Quetzacoalt91 <thomas.nabord@prestashop.com>
2014-03-24 11:43:29 +0100

    / MO blockviewed : ps_versions_compliancy modified (1.5.6.1 => 1.6)

--- blockviewed.php
+++ blockviewed.php
@@ -43,7 +43,7 @@ class BlockViewed extends Module
 
 		$this->displayName = $this->l('Viewed products block');
 		$this->description = $this->l('Adds a block displaying recently viewed products.');
-		$this->ps_versions_compliancy = array('min' => '1.5.6.1', 'max' => _PS_VERSION_);
+		$this->ps_versions_compliancy = array('min' => '1.6', 'max' => _PS_VERSION_);
 	}
 
 	public function install()
